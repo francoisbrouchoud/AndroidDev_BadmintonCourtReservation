9f77bd402a9c30ad2afb8b6adb9b02e444d672a4
delbuonoluca <luca.delbuono@students.hevs.ch>
2022-10-26 20:09:31 +0200

    Reservation activity navigation

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/reservation/CourtReservationActivity.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/reservation/CourtReservationActivity.java
@@ -1,37 +1,8 @@
 package com.example.androiddev_badmintoncourtreservation.ui.reservation;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import com.example.androiddev_badmintoncourtreservation.ui.BaseActivity;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+public class CourtReservationActivity extends BaseActivity {
 
-import com.example.androiddev_badmintoncourtreservation.databinding.FragmentSlideshowBinding;
 
-public class CourtReservationActivity extends Fragment {
-
-    private FragmentSlideshowBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ReservationsActivity_old reservationsActivity =
-                new ViewModelProvider(this).get(ReservationsActivity_old.class);
-
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSlideshow;
-        reservationsActivity.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
