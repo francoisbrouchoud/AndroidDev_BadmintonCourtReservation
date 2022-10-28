743f4cf29d8b8f097c03aa2762bf8e0b7f360dca
François Brouchoud <francois@brouchoud.ch>
2022-10-28 09:02:56 +0200

    feat: PlayerDao queries

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/dao/PlayerDao.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/dao/PlayerDao.java
@@ -1,4 +1,26 @@
 package com.example.androiddev_badmintoncourtreservation.database.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.androiddev_badmintoncourtreservation.database.entity.PlayerEntity;
+
+import java.util.List;
+
+@Dao
 public interface PlayerDao {
+    @Query("SELECT * FROM players")
+    public abstract LiveData<List<PlayerEntity>> getAll();
+
+    @Insert
+    public abstract long insert(PlayerEntity player);
+
+    @Update
+    public abstract void update(PlayerEntity player);
+
+    @Query("DELETE FROM players")
+    public abstract void deleteAll();
 }
