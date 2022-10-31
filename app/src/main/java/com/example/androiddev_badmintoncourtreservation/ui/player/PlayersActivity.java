a94d2e1bbf28cc79d0852013bc72d4f597a9f9f4
delbuonoluca <luca.delbuono@students.hevs.ch>
2022-10-31 11:33:53 +0100

    Complete UI PlayersActivity

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/player/PlayersActivity.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/player/PlayersActivity.java
@@ -1,52 +1,79 @@
 package com.example.androiddev_badmintoncourtreservation.ui.player;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.androiddev_badmintoncourtreservation.R;
 import com.example.androiddev_badmintoncourtreservation.adapter.PlayersRecycleAdapter;
 import com.example.androiddev_badmintoncourtreservation.database.entity.PlayerEntity;
 import com.example.androiddev_badmintoncourtreservation.ui.BaseActivity;
+import com.example.androiddev_badmintoncourtreservation.util.OnAsyncEventListener;
 import com.example.androiddev_badmintoncourtreservation.util.RecyclerViewItemClickListener;
+import com.example.androiddev_badmintoncourtreservation.viewmodel.player.PlayerListViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class PlayersActivity extends BaseActivity {
 
     private PlayersRecycleAdapter<PlayerEntity> adapter;
-    RecyclerView recyclerView;
-    FloatingActionButton fab;
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
 
+    private PlayerListViewModel listViewModel;
+    private List<PlayerEntity> players;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         getLayoutInflater().inflate(R.layout.activity_players, frameLayout);
-        setTitle(R.string.playersActivity_homePage);
         navigationView.setCheckedItem(R.id.nav_none);
 
-        recyclerView = findViewById(R.id.PlayerRv);
+        setTitle(R.string.playersActivity_homePage);
 
+        recyclerView = findViewById(R.id.PlayerRv);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-
         adapter = new PlayersRecycleAdapter<>(new RecyclerViewItemClickListener() {
             @Override
             public void onItemClick(View v, int position) {
                 //Edit the player
+                Intent intent = new Intent(PlayersActivity.this, EditPlayerActivity.class);
+                intent.setFlags(
+                        Intent.FLAG_ACTIVITY_NO_ANIMATION |
+                                Intent.FLAG_ACTIVITY_NO_HISTORY
+                );
+                intent.putExtra("playerId", players.get(position).getId());
+                startActivity(intent);
             }
 
             @Override
             public void onItemLongClick(View v, int position) {
                 //Delete the player
+                deletePlayerDialog(position);
+            }
+        });
+
+        PlayerListViewModel. Factory factory = new PlayerListViewModel.Factory(getApplication());
+        listViewModel = new ViewModelProvider(this, (ViewModelProvider.Factory) factory).get(PlayerListViewModel.class);
+        listViewModel.getPlayers().observe(this, playerEntities -> {
+            if(playerEntities != null){
+                players = playerEntities;
+                adapter.setData(players);
             }
         });
 
@@ -70,10 +97,40 @@ public class PlayersActivity extends BaseActivity {
             drawerLayout.closeDrawer(GravityCompat.START);
             return false;
         }
-        /*
-        The activity has to be finished manually in order to guarantee the navigation hierarchy working.
-        */
+
         finish();
         return super.onNavigationItemSelected(item);
+    }
+
+    private void deletePlayerDialog(final int position){
+        final PlayerEntity player = players.get(position);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        final View view = inflater.inflate(R.layout.row_delete_item, null);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(R.string.alert_playersActivity_deleteTitle);
+        alertDialog.setCancelable(false);
+        final TextView tvDeleteMessage = view.findViewById(R.id.tv_delete_item);
+        tvDeleteMessage.setText(String.format(getString(R.string.alert_playersActivity_deleteMessage), player.getFirstname()));
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.alert_playersActivity_btnPositive), (dialog, which) -> {
+            Toast toast = Toast.makeText(this, getString(R.string.toast_playersActivity_delete), Toast.LENGTH_LONG);
+            listViewModel.deletePlayer(player, new OnAsyncEventListener() {
+                @Override
+                public void onSuccess() {
+                    //log success
+                }
+
+                @Override
+                public void onFailure(Exception e) {
+                    //log failure
+                }
+            });
+            toast.show();
+        });
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.alert_playersActivity_btnNegative), (dialog, which) -> alertDialog.dismiss());
+        alertDialog.setView(view);
+        alertDialog.show();
     }
 }
