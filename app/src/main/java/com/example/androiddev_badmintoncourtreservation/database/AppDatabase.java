5011cc195057ddacc71e2f3ed769e446fa188d38
François Brouchoud <francois@brouchoud.ch>
2022-10-31 08:54:23 +0100

    feat: create AppDatabase

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/AppDatabase.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/AppDatabase.java
@@ -1,6 +1,92 @@
 package com.example.androiddev_badmintoncourtreservation.database;
-//TODO add data
-public abstract class AppDatabase {
 
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.androiddev_badmintoncourtreservation.database.dao.PlayerDao;
+import com.example.androiddev_badmintoncourtreservation.database.entity.CourtEntity;
+import com.example.androiddev_badmintoncourtreservation.database.entity.PlayerEntity;
+import com.example.androiddev_badmintoncourtreservation.database.entity.ReservationEntity;
+
+import java.util.concurrent.Executors;
+
+//TODO add data
+@Database(entities = {PlayerEntity.class, CourtEntity.class, ReservationEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase{
+
+    private static final String TAG = "AppDatabase";
+    private static AppDatabase instance;
+    private static final String DATABASE_NAME = "bank-database";
+
+    public abstract PlayerDao playerDao();
+    private final MutableLiveData<Boolean> mIsDatabaseCreated = new MutableLiveData<>();
+
+    public static AppDatabase getInstance(final Context context) {
+        if (instance == null) {
+            synchronized (AppDatabase.class) {
+                if (instance == null) {
+                    instance = buildDatabase(context.getApplicationContext());
+                    instance.updateDatabaseCreated(context.getApplicationContext());
+                }
+            }
+        }
+        return instance;
+    }
+
+    private static AppDatabase buildDatabase(final Context appContext) {
+        Log.i(TAG, "Database will be initialized.");
+        return Room.databaseBuilder(appContext, AppDatabase.class, DATABASE_NAME)
+                .addCallback(new Callback() {
+                    @Override
+                    public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                        super.onCreate(db);
+                        Executors.newSingleThreadExecutor().execute(() -> {
+                            AppDatabase database = AppDatabase.getInstance(appContext);
+                            initializeDemoData(database);
+                            // notify that the database was created and it's ready to be used
+                            database.setDatabaseCreated();
+                        });
+                    }
+                }).build();
+    }
+
+    public static void initializeDemoData(final AppDatabase database) {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            database.runInTransaction(() -> {
+                Log.i(TAG, "Wipe database.");
+                database.playerDao().deleteAll();
+                //database.courtDao().deleteAll();
+                //database.reservationDao().deleteAll();
+
+                DatabaseInitializer.populateDatabase(database);
+            });
+        });
+    }
+
+    /**
+     * Check whether the database already exists and expose it via {@link #getDatabaseCreated()}
+     */
+    private void updateDatabaseCreated(final Context context) {
+        if (context.getDatabasePath(DATABASE_NAME).exists()) {
+            Log.i(TAG, "Database initialized.");
+            setDatabaseCreated();
+        }
+    }
+
+    private void setDatabaseCreated(){
+        mIsDatabaseCreated.postValue(true);
+    }
+
+    public LiveData<Boolean> getDatabaseCreated() {
+        return mIsDatabaseCreated;
+    }
 
 }
