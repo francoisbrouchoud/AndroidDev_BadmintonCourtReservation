221bcd30935ad9422232a9b6c6d88d999f8de5d6
François Brouchoud <francois@brouchoud.ch>
2022-10-31 10:33:50 +0100

    feat: create CRUD operations

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/async/player/DeletePlayer.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/async/player/DeletePlayer.java
@@ -1,4 +1,42 @@
 package com.example.androiddev_badmintoncourtreservation.database.async.player;
 
-public class DeletePlayer {
+import android.app.Application;
+import android.os.AsyncTask;
+
+import com.example.androiddev_badmintoncourtreservation.BaseApp;
+import com.example.androiddev_badmintoncourtreservation.database.entity.PlayerEntity;
+import com.example.androiddev_badmintoncourtreservation.util.OnAsyncEventListener;
+
+public class DeletePlayer extends AsyncTask<PlayerEntity, Void, Void> {
+    private Application application;
+    private OnAsyncEventListener callback;
+    private Exception exception;
+
+    public DeletePlayer(Application application, OnAsyncEventListener callback){
+        this.application = application;
+        this.callback = callback;
+    }
+
+    @Override
+    protected Void doInBackground(PlayerEntity... params) {
+        try {
+            for(PlayerEntity player : params){
+                ((BaseApp) application).getDatabase().playerDao().delete(player);
+            }
+        } catch (Exception e){
+            exception = e;
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid){
+        if(callback != null){
+            if(exception == null){
+                callback.onSuccess();
+            } else {
+                callback.onFailure(exception);
+            }
+        }
+    }
 }
