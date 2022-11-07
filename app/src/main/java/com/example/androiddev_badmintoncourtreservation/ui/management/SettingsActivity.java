1d4b1d757018e9e38e67f8a6dc4990345369e666
delbuonoluca <luca.delbuono@students.hevs.ch>
2022-11-07 08:32:29 +0100

    feature: settings activity

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/management/SettingsActivity.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/management/SettingsActivity.java
@@ -1,16 +1,83 @@
 package com.example.androiddev_badmintoncourtreservation.ui.management;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.PreferenceFragmentCompat;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.androiddev_badmintoncourtreservation.R;
+import com.example.androiddev_badmintoncourtreservation.ui.BaseActivity;
+
+import java.util.List;
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        /*getFragmentManager().beginTransaction()
+                .replace(android.R.id.content,new LanguageSettingFragment())
+                .commit();*/
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, new LanguageSettingFragment())
+                    .commit();
+        }
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        changeLanguage(sharedPrefs.getString("pref_languages", "fr"));
+
     }
+
+
+    public static class DarkModeSettingFragment extends PreferenceFragmentCompat{
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+
+
+
+        }
+    }
+
+    public static class LanguageSettingFragment extends PreferenceFragmentCompat{
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
+    }
+
+    public void changeLanguage(String lang){
+        Locale myLocale = new Locale(lang);
+        Locale.setDefault(myLocale);
+        android.content.res.Configuration config = new android.content.res.Configuration();
+        config.locale = myLocale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+
 }
