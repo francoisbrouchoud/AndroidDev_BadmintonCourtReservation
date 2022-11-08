b5e0513d6d81fd3460fb15e66de7825a2a29ffb5
François Brouchoud <francois@brouchoud.ch>
2022-11-08 07:48:15 +0100

    feat: Add court DAO with CRUD methods

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/dao/CourtDao.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/dao/CourtDao.java
@@ -1,4 +1,33 @@
 package com.example.androiddev_badmintoncourtreservation.database.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.androiddev_badmintoncourtreservation.database.entity.CourtEntity;
+
+import java.util.List;
+
+@Dao
 public interface CourtDao {
+    @Query("SELECT * FROM courts WHERE id = :id")
+    public abstract LiveData<CourtEntity> getById(Long id);
+
+    @Query("SELECT * FROM courts")
+    public abstract LiveData<List<CourtEntity>> getAll();
+
+    @Insert
+    public abstract long insert(CourtEntity courtEntity);
+
+    @Update
+    public abstract void update(CourtEntity courtEntity);
+
+    @Query("DELETE FROM courts")
+    public abstract void deleteAll();
+
+    @Delete
+    void delete(CourtEntity court);
 }
