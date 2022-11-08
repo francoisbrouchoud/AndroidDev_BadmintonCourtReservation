4ee4bca49a8563200da91515e12f71e2b40cfbcf
François Brouchoud <francois@brouchoud.ch>
2022-11-08 07:57:04 +0100

    feat: add sample data for courts

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/DatabaseInitializer.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/database/DatabaseInitializer.java
@@ -3,6 +3,7 @@ package com.example.androiddev_badmintoncourtreservation.database;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.androiddev_badmintoncourtreservation.database.entity.CourtEntity;
 import com.example.androiddev_badmintoncourtreservation.database.entity.PlayerEntity;
 
 import java.util.Date;
@@ -19,6 +20,11 @@ public class DatabaseInitializer {
     private static void addPlayer(final AppDatabase db, final String firstname, final String lastname, final String birthdate, final String gender, final String phone, final String address, final String place, final int level){
         PlayerEntity player = new PlayerEntity(firstname, lastname, birthdate, gender, phone, address, place, level);
         db.playerDao().insert(player);
+    }
+
+    private static void addCourt(final AppDatabase db, final String courtsName, String description, String address, String place, String imagePath, double hourlyPrice){
+        CourtEntity court = new CourtEntity(courtsName, description, address, place, imagePath, hourlyPrice);
+
     }
 
     private static void populateWithTestData(AppDatabase db) {
@@ -40,6 +46,11 @@ public class DatabaseInitializer {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        addCourt(db, "Badminton des Iles", "Terrain avec fort éclairage", "Les Iles", "1950 Sion", "imageSion.png", 20);
+        addCourt(db, "Badminton de Chippis", "claqué au sol", "L'Usine 41", "Chippis", "pasbeau.png", 10);
+
+
 
     }
 
