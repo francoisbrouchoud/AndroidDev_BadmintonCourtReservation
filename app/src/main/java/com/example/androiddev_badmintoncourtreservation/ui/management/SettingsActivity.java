75166a02db9130f3c4c24d9a09c088f3317c7602
François Brouchoud <francois@brouchoud.ch>
2022-11-14 15:55:18 +0100

    feat: about launch button

--- app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/management/SettingsActivity.java
+++ app/src/main/java/com/example/androiddev_badmintoncourtreservation/ui/management/SettingsActivity.java
@@ -9,16 +9,23 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.androiddev_badmintoncourtreservation.R;
 import com.example.androiddev_badmintoncourtreservation.ui.court.CourtsActivity;
+import com.example.androiddev_badmintoncourtreservation.ui.court.EditCourtActivity;
+import com.example.androiddev_badmintoncourtreservation.ui.player.PlayersActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class SettingsActivity extends AppCompatActivity {
 
     protected SharedPreferences sharedPreferences;
-
+    Button aboutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +46,20 @@ public class SettingsActivity extends AppCompatActivity {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         else
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
+
+        aboutButton = (Button) findViewById(R.id.action_about);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 
     public static class DarkModeSettingFragment extends PreferenceFragmentCompat{
@@ -47,5 +68,6 @@ public class SettingsActivity extends AppCompatActivity {
             setPreferencesFromResource(R.xml.preferences, rootKey);
         }
     }
+
 
 }
