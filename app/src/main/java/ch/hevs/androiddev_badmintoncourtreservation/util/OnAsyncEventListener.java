430373a8b0cdadb3b9a0adbf3483d088e42b76b2
luca.delbuono <luca.delbuono@students.hevs.ch>
2022-11-18 09:42:42 +0100

    fix: util classes comment & clean code

--- app/src/main/java/ch/hevs/androiddev_badmintoncourtreservation/util/OnAsyncEventListener.java
+++ app/src/main/java/ch/hevs/androiddev_badmintoncourtreservation/util/OnAsyncEventListener.java
@@ -1,6 +1,8 @@
 package ch.hevs.androiddev_badmintoncourtreservation.util;
 
-
+/**
+ * Listener of the async event tasks. Used as callback on operation involving the DB.
+ */
 public interface OnAsyncEventListener {
     void onSuccess();
     void onFailure(Exception e);
