effdd897ef9c9579dca422ea034f985459d92974
François Brouchoud <francois@brouchoud.ch>
2022-12-10 14:58:03 +0100

    feat: adapt POJO ReservationWithPlayerAndCourt for Firebase integration

--- app/src/main/java/ch/brouchoud/androiddev_badmintoncourtreservation/database/pojo/ReservationWithPlayerAndCourt.java
+++ app/src/main/java/ch/brouchoud/androiddev_badmintoncourtreservation/database/pojo/ReservationWithPlayerAndCourt.java
@@ -11,13 +11,8 @@ import ch.brouchoud.androiddev_badmintoncourtreservation.database.entity.Reserva
  * Table reservation has relation with player and court
  */
 public class ReservationWithPlayerAndCourt {
-
-    @Embedded
     public ReservationEntity reservation;
 
-    @Relation(parentColumn = "playerId", entityColumn = "id", entity = PlayerEntity.class)
     public PlayerEntity player;
-
-    @Relation(parentColumn = "courtId", entityColumn = "id", entity = CourtEntity.class)
     public CourtEntity court;
 }
