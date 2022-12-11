b9ac516e1c9984144baf5d7660594aaccd82045a
delbuonoluca <luca.delbuono@students.hevs.ch>
2022-12-11 09:28:24 +0100

    feat: remove room classes

--- app/src/main/java/ch/brouchoud/androiddev_badmintoncourtreservation/BaseApp.java
+++ app/src/main/java/ch/brouchoud/androiddev_badmintoncourtreservation/BaseApp.java
@@ -1,21 +1,21 @@
 package ch.brouchoud.androiddev_badmintoncourtreservation;
 
 import android.app.Application;
-import ch.brouchoud.androiddev_badmintoncourtreservation.database.AppDatabase;
+
 import ch.brouchoud.androiddev_badmintoncourtreservation.database.repository.CourtRepository;
 import ch.brouchoud.androiddev_badmintoncourtreservation.database.repository.PlayerRepository;
 import ch.brouchoud.androiddev_badmintoncourtreservation.database.repository.ReservationRepository;
 
 public class BaseApp extends Application {
 
-    @Override
+    /*@Override
     public void onCreate() {
         super.onCreate();
-    }
+    }*/
 
-    public AppDatabase getDatabase() {
+    /*public AppDatabase getDatabase() {
         return AppDatabase.getInstance(this);
-    }
+    }*/
 
     public PlayerRepository getPlayerRepository() {
         return PlayerRepository.getInstance();
